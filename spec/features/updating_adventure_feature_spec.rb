0e44e31e1f9ebf304af29065aeb4072c54e066ad
Marlon Date-Chong <mdatechong@gmail.com>
2016-08-16 12:28:07 +0100

    updated tests

--- spec/features/updating_adventure_feature_spec.rb
+++ spec/features/updating_adventure_feature_spec.rb
@@ -11,7 +11,7 @@ feature "Updating adventures" do
 
     fill_in "Title", with: "Marlon's story"
     fill_in "Synopsis", with: "Marlon buy trousers"
-    attach_file 'Image', "/Users/jojograndjojo/Projects2/TFAA/lib/assets/owl jolson.jpeg"
+    attach_file 'Image', "app/assets/images/Bristol.jpeg"
     click_button "submit"
 
 
@@ -29,7 +29,7 @@ feature "Updating adventures" do
 
     fill_in "Title", with: "Marlon's story"
     fill_in "Synopsis", with: "Marlon buy trousers"
-    attach_file 'Image', "/Users/jojograndjojo/Projects2/TFAA/lib/assets/owl jolson.jpeg"
+    attach_file 'Image', "app/assets/images/Bristol.jpeg"
     click_button "submit"
 
 
@@ -50,7 +50,7 @@ feature "Updating adventures" do
     click_link "Create"
     fill_in "Title", with: "Marlon's story"
     fill_in "Synopsis", with: "Marlon buy trousers"
-    attach_file 'Image', "/Users/jojograndjojo/Projects2/TFAA/lib/assets/owl jolson.jpeg"
+    attach_file 'Image', "app/assets/images/Bristol.jpeg"
     click_button "submit"
 
 
